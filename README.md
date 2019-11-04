f45177e15725ab4f307d523cc414bc243d3755ea
Ian Charawe <Iancharawe94@gmail.com>
2019-11-04 17:13:02 +0300

    changes in html css, js

---
+++ README.md
@@ -0,0 +1,26 @@
+# Delani Studio site
+## Ian charawe
+## Description
+## Deployment
+
+## Technology used
+HTML
+CSS
+Bootstrap
+Jquery
+Javascript
+## BDD
+<img src="images/delani.jpg" alt="picture">
+- project is about delani studio design and has one page
+- About us explains the function of the website
+- Services shows what services delani offers
+- What we do is a section that allow users to toggle through the functions
+- when it comes to the email the website is responsive and gives a feedback depending on the entry
+
+## BUGS
+it contains bugs in css and html
+## Support and contact details
+Iancharawe94@gmail.com
+254 27 487 476
+## License
+MIT license
--- js/script.js
+++ js/script.js
@@ -34,15 +34,11 @@ $(document).ready(function() {
     $('#lego-4-5').show();
   });
   $('#lego-4-6').click(function(){
-    $('#lego-4-5').hide();
+    $('#lego-4-6').hide();
   });
-  $('#lego-4-5').click(function(){
-    $('#lego-4-5').show();
-  });
-  $('#lego-4-6').click(function(){
-    $('#lego-4-5').show();
-  });
+  
 });
+
 
  
 
@@ -51,16 +47,19 @@ $(document).ready(function() {
 
 
 
+$(document).ready(function(){
 
-function functiongmail(){
-var name = document.getElementById("name").value;
-var Email = document.getElementById("email").value;
-var Message = document.getElementById("message").value;
 
+$('#submit').click(function () {
+var name = $('#name').val();
+var Email = $('#email').val();
+var Message = $('#message');
+event.preventDefault();
 if (name == '' || Email == '' || Message == '' ){
   alert('Please make sure you have filled in correctly ');
 } else {
   alert ('Hi'+ name + 'we have received your mesage. Thank you for reacting out to us.');
 }
 
-}
+});
+});
