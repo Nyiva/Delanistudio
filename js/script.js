767a576013a19765f33746b34e240a7c6ea9642c
Ian Charawe <Iancharawe94@gmail.com>
2019-11-04 09:15:42 +0300

    submit button,color added and java script change in code

--- js/script.js
+++ js/script.js
@@ -2,35 +2,59 @@ $(document).ready(function() {
   $('#lego-4-1').click(function(){
     $('#lego-4-2').show();
   });
-  $('#lego-4-1').click(function(){
-    $('#lego-4-2').hide();
-  });
-  $('#lego-4-1').click(function(){
-    $('#lego-4-3').hide();
-  });
-  $('#lego-4-1').click(function(){
-    $('#lego-4-3').show();
-  });
-  $('#lego-4-1').click(function(){
-    $('#lego-4-3').hide();
-  });
-  $('#lego-4-1').click(function(){
-    $('#lego-4-4').show();
-  });
-  $('#lego-4-1').click(function(){
-    $('#lego-4-4').hide();
+    $('#lego-4-1').click(function(){
+      $('#lego-4-1').hide();
+    });
+    $('#lego-4-2').click(function(){
+      $('#lego-4-1').show();
+    });
+    $('#lego-4-2').click(function(){
+      $('#lego-4-2').hide();
   });
   $('#lego-4-3').click(function(){
-    $('#toggle').show();
+    $('#lego-4-4').show();
   });
   $('#lego-4-3').click(function(){
     $('#lego-4-3').hide();
   });
   $('#lego-4-4').click(function(){
-    $('#toggle').show();
+    $('#lego-4-3').show();
   });
   $('#lego-4-4').click(function(){
     $('#lego-4-4').hide();
   });
 
+  $('#lego-4-5').click(function(){
+    $('#lego-4-6').show();
+  });
+  $('#lego-4-5').click(function(){
+    $('#lego-4-5').hide();
+  });
+  $('#lego-4-6').click(function(){
+    $('#lego-4-5').show();
+  });
+  $('#lego-4-6').click(function(){
+    $('#lego-4-5').hide();
+  });
+  $('#lego-4-6').click(function(){
+    $('#lego-4-5').show();
+  });
+
 });
+
+ 
+
+
+
+
+
+
+
+function functiongmail(){
+var name = document.getElementById("name").value;
+var name = document.getElementById("email").value;
+var name = document.getElementById("message").value;
+
+
+
+}
